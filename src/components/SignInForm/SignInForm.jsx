a623a99b8e1f6558e3fc8382c21c31937932a049
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-03-07 21:37:04 +0100

    React for page

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,71 +1,84 @@
 import React from 'react';
 import { LuEyeOff } from 'react-icons/lu';
-// import { useForm } from 'react-hook-form';
-// import { yupResolver } from '@hookform/resolvers/yup';
-// import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import * as yup from 'yup';
 import s from './SignInForm.module.css';
 import Logo from '../Logo/Logo.jsx';
 
-// const schema = yup.object().shape({
-//   email: yup
-//     .string()
-//     .email('Invalid email address')
-//     .required('Email is required'),
-//   password: yup
-//     .string()
-//     .min(6, 'Password must be at least 6 characters')
-//     .required('Password is required'),
-// });
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+});
 
 const SignInForm = () => {
-  //   const {
-  //     register,
-  //     handleSubmit,
-  //     formState: { errors },
-  //   } = useForm({ resolver: yupResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
-  //   const onSubmit = (data) => {
-  //     console.log(data);
-  //     alert('Form submitted successfully!');
-  //   };
+  const onSubmit = (data) => {
+    console.log(data);
+    alert('Form submitted successfully!');
+  };
 
   return (
-    
-          <div className={s.container}>
-              <div className={s.logo_container}>
-                  <Logo/>
-              </div>
-              
-        <div className={s.menu_container}>
-          <h2 className={s.title}>Sign In</h2>
-          <form className={s.form}>
-            <div className={s.inputGroup}>
-              <label>Email</label>
-              <input type="text" placeholder="Enter your email" />
-            </div>
+    <div className={s.container}>
+      <div className={s.logo_container}>
+        <Logo />
+      </div>
 
-            <div className={s.inputGroup}>
-              <label>Password</label>
-              <div className={s.icon}>
-                <input type="password" placeholder="Enter your password" />
-                <LuEyeOff className={s.eyeIcon} />
-              </div>
-            </div>
+      <div className={s.menu_container}>
+        <h2 className={s.title}>Sign In</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+          <div className={s.inputGroup}>
+            <label>Email</label>
+            <input
+              type="text"
+              {...register('email')}
+              placeholder="Enter your email"
+            />
 
-            <button type="submit" className={s.button}>
-              Sign In
-            </button>
-          </form>
-
-          <div className={s.wrapperUp}>
-            <p className={s.account}>Don’t have an account?&nbsp;</p>
-            <a href="/signup" className={s.signup}>
-              Sign Up
-            </a>
+            {errors.email && <p className={s.error}>{errors.email.message}</p>}
           </div>
+
+          <div className={s.inputGroup}>
+            <label>Password</label>
+            <div className={s.icon}>
+              <input
+                type="password"
+                {...register('password')}
+                placeholder="Enter your password"
+              />
+              {errors.password && (
+                <p className={s.error}>{errors.password.message}</p>
+              )}
+              <LuEyeOff className={s.eyeIcon} />
+            </div>
+          </div>
+
+          <button type="submit" className={s.button}>
+            Sign In
+          </button>
+        </form>
+
+        <div className={s.wrapperUp}>
+          <p className={s.account}>Don’t have an account?&nbsp;</p>
+          <a href="/signup" className={s.signup}>
+            Sign Up
+          </a>
         </div>
       </div>
-    
+    </div>
   );
 };
 
