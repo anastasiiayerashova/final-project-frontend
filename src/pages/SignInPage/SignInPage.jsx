b42ef7c82523cd7872035e69166bd49d1d60171c
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-03-08 17:59:08 +0100

    fix css

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import SignInForm from '../../components/SignInForm/SignInForm';
+import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection.jsx';
+import s from './SignInPage.module.css'
 
 const SignInPage = () => {
   return (
-    
-      <SignInForm />
+    <div className={s.main_wrapper}>
+      <div>
+        <SignInForm />
+      </div>
+      <div className={s.advantages_wrapper}>
+        <AdvantagesSection/>
+      </div>
+    </div>
+      
     
   );
 };
