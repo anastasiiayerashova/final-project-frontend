78acd08d4bb0d39dd371211f9ba142fc99c6ac6b
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-03-09 18:09:16 +0100

    clear some files

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,14 +1,1 @@
-import Logo from '../../components/Logo/Logo';
-import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import s from './SignUpPage.module.css';
-function SignUpPage() {
-  return (
-    <div className={s.container}>
-      <Logo />
-      <h1 className={s.title}>Sign Up</h1>
-      <SignUpForm />
-    </div>
-  );
-}
 
-export default SignUpPage;
