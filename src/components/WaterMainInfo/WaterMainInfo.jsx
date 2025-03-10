69de9de965ffca1dc64f2a137574daa3597cfd7f
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-03-11 00:26:48 +0100

    add water form validation

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,37 +1,28 @@
-import { useState } from "react";
 import AddWaterBtn from "../AddWaterBtn/AddWaterBtn.jsx";
 import Logo from "../Logo/Logo.jsx";
 import WaterDailyNorma from "../WaterDailyNorma/WaterDailyNorma.jsx";
 import WaterProgressBar from "../WaterProgressBar/WaterProgressBar.jsx";
 import s from "./WaterMainInfo.module.css";
-import WaterModal from "../WaterModal/WaterModal.jsx";
+import { TYPE } from "../../constants/index.js";
 
-const WaterMainInfo = () => {
-    const [modalIsOpen, setIsOpen] = useState(false);
-
-    function openModal() {
-    setIsOpen(true);
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
+const WaterMainInfo = ({ openWaterModal }) => {
+    
+    const handleOpenAddWaterModal = () => {
+        openWaterModal({isOpen: true, type: TYPE.ADD_WATER})
+    }
+    
     return (
         <div className={s.waterContainer}>
-            
-                <div className={s.logo}><Logo /></div>
-    
-                
-                
-                    <WaterDailyNorma/>
-            
+            <div className={s.logo}>
+                <Logo />
+            </div>
+                <WaterDailyNorma/>
             <div className={s.progress}>
                 <WaterProgressBar/>
             </div>
             <div className={s.btn}>
-                <AddWaterBtn onClick={openModal} />
+                <AddWaterBtn onClick={handleOpenAddWaterModal} />
             </div>
-            {modalIsOpen && <WaterModal closeModal={closeModal} />}
         </div>
     )
 }
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,87 +1,25 @@
 import { useEffect, useState } from "react";
 import s from "./WaterModal.module.css";
+import WaterForm from "../WaterForm/WaterForm.jsx";
+import { TYPE, MODAL_NAME } from "../../constants/index.js";
 
-const WaterModal = ({ closeModal }) => {
-    const [amount, setAmount] = useState(50);
-    const [isDisabled, setIsDisabled] = useState(false)
-
-    const handleStepChange = (step) => {
-        setAmount((prevAmount) => {
-            const newAmount = prevAmount + step;
-            return Math.min(5000, Math.max(0, newAmount)); // ограничиваем 0 - 5000
-        });
-    };
-
-    const handleInputChange = (e) => {
-        let value = Number(e.target.value);
-        if (isNaN(value)) return; // защита от NaN
-
-        value = Math.min(5000, Math.max(0, value)); // ограничиваем диапазон
-        setAmount(value);
-    };
-
- useEffect(() => {
-  document.body.style.overflow = "hidden";
-
-  return () => {
-    document.body.style.overflow = "";
-  };
-}, []);
+const WaterModal = ({ onClose, type }) => {
+    
     return (
-        <div className={s.backdrop} onClick={closeModal}>
-            <div className={s.modal} onClick={(e) => e.stopPropagation()}>
-                <button className={s.closeBtn} onClick={closeModal}>
-                    <svg className={s.iconX}>
-                        <use href='../../../public/sprite.svg#x'></use>
-                    </svg>
-                </button>
-                <h2 className={s.title}>Edit the entered amount <br/> of water</h2>
-                <p className={s.subtitle}>Correct entered data:</p>
-                <form>
-                <div className={s.inputGroup}>
-                     <label htmlFor="amount" className={s.descrAmount}>
-                            Amount of water:
-                        </label>
-                    <div className={s.controls}>
-                        
-                         <button className={s.minusButton} disabled type="button" onClick={() => handleStepChange(-50)}>
-                             <svg className={s.iconMinus}>
-                                <use href='../../../public/sprite.svg#minus_plus'></use>
-                            </svg>
-                        </button>
-                        <input className={s.input} id="amount" value={`${amount} ml`} readOnly />
-                        <button className={s.plusButton} type="button" onClick={() => handleStepChange(50)}>
-                              <svg className={s.iconPlus}>
-                                <use href='../../../public/sprite.svg#plus_minuc'></use>
-                                </svg>
-                        </button>
-                        </div>
-                </div>
-                <div className={s.inputGroup}>
-                <label  htmlFor="timeInput" className={s.timeLabel}>Recording time:</label>
-                <input  className={s.timeInput}
-                    id="timeInput"
-                    type="text"
-                    placeholder='7:00' //add time
-                     />
-                    </div>
-                    <div className={s.inputGroup}>
-                <label htmlFor="waterInput" className={s.manualInput}>
-                    Enter the value of the water used:
-                    </label>
-                    <input 
-                        type="number"
-                        id="waterInput"
-                        className={s.waterInput}
-                    placeholder='250' 
-                    value = {amount}
-                        onChange={handleInputChange}
-                        />
-                        </div>
-                    <button className={s.saveBtn}>Save</button>
-                </form>
-            </div>
-        </div>
+        <>
+            <h2 className={s.title}>
+            {type === TYPE.ADD_WATER ? (
+             'Add water') : (
+            <>
+             Edit the entered amount <br /> of water
+            </>
+)}
+            </h2>
+            <p className={s.subtitle}>
+                {type === TYPE.ADD_WATER ? 'Correct entered data:' : 'Choose a value'}
+            </p>
+            <WaterForm/>
+        </>
     );
 };
 
