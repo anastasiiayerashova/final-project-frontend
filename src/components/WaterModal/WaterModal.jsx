55bc7de003cd104b27c256f2a30137ba13b10818
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-03-10 17:08:29 +0100

    fix css

--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import s from "./WaterModal.module.css";
 
 const WaterModal = ({ closeModal }) => {
-    const [amount, setAmount] = useState(250);
+    const [amount, setAmount] = useState(50);
+    const [isDisabled, setIsDisabled] = useState(false)
 
     const handleStepChange = (step) => {
         setAmount((prevAmount) => {
@@ -36,34 +37,35 @@ const WaterModal = ({ closeModal }) => {
                 </button>
                 <h2 className={s.title}>Edit the entered amount <br/> of water</h2>
                 <p className={s.subtitle}>Correct entered data:</p>
-                
-                <div className={s.amountWrap}>
+                <form>
+                <div className={s.inputGroup}>
                      <label htmlFor="amount" className={s.descrAmount}>
                             Amount of water:
                         </label>
                     <div className={s.controls}>
                         
-                         <button className={s.minus} type="button" onClick={() => handleStepChange(-50)}>
+                         <button className={s.minusButton} disabled type="button" onClick={() => handleStepChange(-50)}>
                              <svg className={s.iconMinus}>
                                 <use href='../../../public/sprite.svg#minus_plus'></use>
                             </svg>
                         </button>
-                        <input className={s.input} id="amount" value={amount} readOnly />
-                        <button className={s.plus} type="button" onClick={() => handleStepChange(50)}>
+                        <input className={s.input} id="amount" value={`${amount} ml`} readOnly />
+                        <button className={s.plusButton} type="button" onClick={() => handleStepChange(50)}>
                               <svg className={s.iconPlus}>
                                 <use href='../../../public/sprite.svg#plus_minuc'></use>
                                 </svg>
                         </button>
-                     </div>
-                
+                        </div>
                 </div>
+                <div className={s.inputGroup}>
                 <label  htmlFor="timeInput" className={s.timeLabel}>Recording time:</label>
                 <input  className={s.timeInput}
                     id="timeInput"
                     type="text"
                     placeholder='7:00' //add time
-                    readOnly />
-                
+                     />
+                    </div>
+                    <div className={s.inputGroup}>
                 <label htmlFor="waterInput" className={s.manualInput}>
                     Enter the value of the water used:
                     </label>
@@ -74,8 +76,10 @@ const WaterModal = ({ closeModal }) => {
                     placeholder='250' 
                     value = {amount}
                         onChange={handleInputChange}
-                    />
+                        />
+                        </div>
                     <button className={s.saveBtn}>Save</button>
+                </form>
             </div>
         </div>
     );
