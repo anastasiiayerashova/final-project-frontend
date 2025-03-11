e91e83315a6f2e0ef9adfda6d2fa971e14592f08
Alex <zemzikova.oleksandra@gmail.com>
2025-03-11 13:40:40 +0200

    Page not found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import s from './NotFoundPage.module.css';
+
 function NotFoundPage() {
-    return (
-        <p>not found page</p>
-    )
+  return (
+    <div className={s.container}>
+      <Logo />
+      <div className={s.container_text}>
+        <h1 className={s.text_number}>404</h1>
+        <h2 className={s.text_title_err}>Page Not Found</h2>
+        <p className={s.text_message}>
+          Sorry, the page you're looking for doesn't exist.
+        </p>
+
+        <Link to="/" className={s.link_home}>
+          Go to Home
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
