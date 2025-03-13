db24ba8fe01b48155cea1521cdf3661f989f21a8
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-03-13 16:35:16 +0100

    add logout

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import s from './LogOutModal.module.css'
 import { MODAL_NAME } from "../../constants/index.js";
+import { logoutUserOperation } from "../../redux/user/operations.js";
 
 const LogOutModal = ({ onClose }) => {
 
   const handleCloseModal = () => onClose(MODAL_NAME.LOGOUT_MODAL)
-  const [isBtnDisabled, setBtnDisabled] = useState(false)
 
   const dispatch = useDispatch()
+
+  const handleLogout = (e) => {
+    dispatch(logoutUserOperation())
+  }
 
   return (
           <div className={s.delete_wrapper}>
@@ -18,7 +22,7 @@ const LogOutModal = ({ onClose }) => {
                 <p>Do you really want to leave?</p>
              </div>
              <div className={s.delete_btns}>
-                <button className={`${s.button} ${s.delete_btn}`}>Log out</button>
+                <button className={`${s.button} ${s.delete_btn}`} onClick={handleLogout}>Log out</button>
                 <button className={`${s.button} ${s.cancelBtn}`} onClick={handleCloseModal}>Cancel</button>
              </div>
           </div>
