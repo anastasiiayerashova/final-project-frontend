d1a76f4fa68b10852aee8607a9d29c80cd3cd1ea
Valentyn Mostovyi <mostovoy.v@gmail.com>
2025-03-16 02:33:21 +0100

    Add margins to chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -119,7 +119,7 @@ const WaterChart = () => {
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart
             data={weeks[currentWeek]}
-            margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
+            margin={{ top: 10, right: 15, left: 0, bottom: 5 }}
           >
             <defs>
               <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
